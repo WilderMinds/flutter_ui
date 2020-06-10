c99cbd74b862e61f29116651a6901c91fcc803d4
Samuel Agbesinyale <elsammyk@gmail.com>
2020-06-10 02:38:57 +0000

    initial commit

---
+++ auth_textfield_effects/android/app/src/main/kotlin/com/samdev/auth_textfield_effects/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.samdev.auth_textfield_effects
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
